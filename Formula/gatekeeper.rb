83ff6c6c6f13e7e5979ed205c76bf6a3f5dd9cf7
retraut <retraut@gmail.com>
2026-01-01 22:29:02 +0100

    chore: update formula to v0.7.0

--- Formula/gatekeeper.rb
+++ Formula/gatekeeper.rb
@@ -2,23 +2,23 @@ class Gatekeeper < Formula
   desc "Service authentication status monitor with daemon, CLI, and tmux integration"
   homepage "https://github.com/retraut/gatekeeper"
   license "MIT"
-  version "0.6.0"
+  version "v0.7.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/retraut/gatekeeper/releases/download/v0.6.0/gatekeeper-darwin-arm64"
-      sha256 "90c13a9edc065adbfe10b735a34ff40bc88cabb59fd68c4e96461a38ce567422"
+      url "https://github.com/retraut/gatekeeper/releases/download/v0.7.0/gatekeeper-darwin-arm64"
+      sha256 "743b91adfc7225e40d3a694f6d97d48e343022fffc4ea57180598ce1e532d501"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/retraut/gatekeeper/releases/download/v0.6.0/gatekeeper-linux-arm64"
-      sha256 "245c578889486da4a06b4d9f063999f4cd7defd1fd68922b362a36e486b84cba"
+      url "https://github.com/retraut/gatekeeper/releases/download/v0.7.0/gatekeeper-linux-arm64"
+      sha256 "49c9160800b14380e513a37c96eae0a5621cbf428cfa113a996018f6e777c2c9"
     end
     on_intel do
-      url "https://github.com/retraut/gatekeeper/releases/download/v0.6.0/gatekeeper-linux-amd64"
-      sha256 "89d16d177b58e12500483ddefb1726217a0c388aaf255831c3db4654f7e0164b"
+      url "https://github.com/retraut/gatekeeper/releases/download/v0.7.0/gatekeeper-linux-amd64"
+      sha256 "4f24076252e271662ecdee28631b4bd36403eb984767867ff51bbb78a818a8a5"
     end
   end
 
@@ -28,7 +28,7 @@ class Gatekeeper < Formula
            else
              "amd64"
            end
-    
+
     os_name = if OS.mac?
                 "darwin"
               elsif OS.linux?
@@ -36,7 +36,7 @@ class Gatekeeper < Formula
               else
                 "unknown"
               end
-    
+
     binary_name = "gatekeeper-#{os_name}-#{arch}"
     bin.install binary_name => "gatekeeper"
   end
